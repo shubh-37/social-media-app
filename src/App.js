377a3253ef95184e6948784c611b90d5fae72165
shubh-37 <shubh.arya37@gmail.com>
2023-06-08 23:28:31 +0530

    added routes to home and landing page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/landing" element={<LandingPage />} />
+      </Routes>
     </div>
   );
 }
