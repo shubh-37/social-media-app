91f67e7f4fa2430a764361ec221079b2df708732
shubh-37 <shubh.arya37@gmail.com>
2023-06-08 23:29:24 +0530

    created a layout of the page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+export default function HomePage(){
+    return(
+      <div>
+        <div className="home-navigate"></div>
+        <div className="main-content"></div>
+        <div className="sidebar"></div>
+      </div>
+    )
+}
