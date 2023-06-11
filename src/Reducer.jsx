8e0e71aad11fd93428676bcb0bda0cccf3641b05
shubh-37 <shubh.arya37@gmail.com>
2023-06-12 01:36:48 +0530

    separate file for reducer function

---
+++ src/Reducer.jsx
@@ -0,0 +1,12 @@
+export default function reducer(state, action) {
+  switch (action.type) {
+    case "GET_ALL_POSTS":
+      return {
+        ...state,
+        allPosts: action.payload,
+        dataRef: action.payload,
+      };
+    default:
+      return {};
+  }
+}
