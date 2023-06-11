d682391c40ce1c09075260c530374a9f88f3c363
shubh-37 <shubh.arya37@gmail.com>
2023-06-12 01:38:22 +0530

    created a reducer and called a dispatch to store all posts in the state

--- src/contexts/PostContextProvider.jsx
+++ src/contexts/PostContextProvider.jsx
@@ -1,21 +1,27 @@
-import { createContext, useEffect} from "react";
+import { useReducer } from "react";
+import { createContext, useEffect } from "react";
+import reducer from "../Reducer";
 
-const postContext = createContext();
+export const postContext = createContext();
 
-export default function PostContextProvider({children}){
-  async function getAllPosts(){
+export default function PostContextProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, {
+    allPosts: [],
+    dataRef: [],
+  });
+  async function getAllPosts() {
     try {
       const response = await fetch("/api/posts");
-      const data = await response.json()
-      console.log(data);
+      const data = await response.json();
+      dispatch({ type: "GET_ALL_POSTS", payload: data.posts });
     } catch (error) {
-      
+      console.log(error);
     }
   }
   useEffect(() => {
     getAllPosts();
-  }, [])
+  }, []);
   return (
-    <postContext.Provider>{children}</postContext.Provider>
-  )
+    <postContext.Provider value={{ state }}>{children}</postContext.Provider>
+  );
 }
