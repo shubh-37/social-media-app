39ead13094bb7e0141db5488c3f386cac30b54e1
shubh-37 <shubh.arya37@gmail.com>
2023-06-13 04:37:07 +0530

    showing all posts saved in the state

--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -1,5 +1,26 @@
+import { useContext } from "react"
+import { postContext } from "../contexts/PostContextProvider"
+
 export default function Explore(){
+  const { state } = useContext(postContext);
+
   return(
-    <h1>this is the explore page</h1>
+    <div className="explore-parent">
+      <h1>this is the explore page</h1>
+      <h2>Showing all posts</h2>
+      <ul>
+            {state?.allPosts?.map((item) => (
+              <li key={item._id}>
+                <h3>{item.username}</h3>
+                <p>{item.content}</p>     
+                <span className="	fa fa-heart-o"></span>
+                <span className="fa fa-comment-o"></span>
+                <span className="fa fa-share-alt"></span>
+                <span className="fa fa-bookmark-o"></span>
+              </li>
+            ))}
+          </ul>
+    </div>
+    
   )
 }
