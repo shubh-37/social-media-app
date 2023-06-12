81440b67e0acaa2fd6a4969b72db412537756bb7
shubh-37 <shubh.arya37@gmail.com>
2023-06-13 04:37:43 +0530

    showing all users on the side bar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,10 +8,18 @@ export default function HomePage() {
   return (
     <div className="home">
       <div className="home-navigate">
-        <NavLink to="/"><span className="fa fa-home">Home</span></NavLink>
-        <NavLink to="/explore"><span className="fa fa-rocket">Explore</span></NavLink>
-        <NavLink to="/bookmark"><span className="fa fa-bookmark-o">Bookmarks</span></NavLink>
-        <NavLink to="/profile"><span className="fa fa-user-o">Profile</span></NavLink>
+        <NavLink to="/">
+          <span className="fa fa-home">Home</span>
+        </NavLink>
+        <NavLink to="/explore">
+          <span className="fa fa-rocket">Explore</span>
+        </NavLink>
+        <NavLink to="/bookmark">
+          <span className="fa fa-bookmark-o">Bookmarks</span>
+        </NavLink>
+        <NavLink to="/profile">
+          <span className="fa fa-user-o">Profile</span>
+        </NavLink>
         <button>Create New post</button>
       </div>
       <div className="main-content">
@@ -24,22 +32,24 @@ export default function HomePage() {
             placeholder="What is Happening!?"
           />
           <div>
-          <span className="fa fa-image"></span>
-          <span className="fa fa-smile-o"></span>
+            <span className="fa fa-image"></span>
+            <span className="fa fa-smile-o"></span>
             <button>Post</button>
           </div>
         </div>
         <div className="latest-post">
-          <h2>Latest Posts</h2>
+          <h2>Will show posts of the following and self</h2>
           <ul>
             {state?.allPosts?.map((item) => (
               <li key={item._id}>
                 <h3>{item.username}</h3>
-                <p>{item.content}</p>     
-                <span className="	fa fa-heart-o"></span>
-                <span className="fa fa-comment-o"></span>
-                <span className="fa fa-share-alt"></span>
-                <span className="fa fa-bookmark-o"></span>
+                <p>{item.content}</p>
+                <div className="post-icons">
+                  <span className="	fa fa-heart-o"></span>
+                  <span className="fa fa-comment-o"></span>
+                  <span className="fa fa-share-alt"></span>
+                  <span className="fa fa-bookmark-o"></span>
+                </div>
               </li>
             ))}
           </ul>
@@ -47,9 +57,22 @@ export default function HomePage() {
       </div>
       <div className="sidebar">
         <label htmlFor="search">Search</label>
-        <input type="text" name="" id="search" placeholder="Search people, post, anything" />
+        <input
+          type="text"
+          name=""
+          id="search"
+          placeholder="Search people, post, anything"
+        />
         <div>
           <h2>Who to follow?</h2>
+          <ul>
+            {state?.allUsers?.map((item) => (
+              <li key={item._id}>
+                {item.username}
+                <button>Follow</button>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
