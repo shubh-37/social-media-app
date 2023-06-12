15dfd605d1ac21f5cf120ad873a6053dbf65c21d
shubh-37 <shubh.arya37@gmail.com>
2023-06-13 04:19:49 +0530

    added 2 new pages

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,5 @@
+export default function Login(){
+  return (
+    <h1>this is the login page</h1>
+  )
+}
---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,5 @@
+export default function SignUp(){
+  return (
+    <h1>This is the sign up page</h1>
+  )
+}
