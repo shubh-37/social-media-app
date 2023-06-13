d6ab1385bc0d8532ec13cc6fde3c0d30c3ad39d6
shubh-37 <shubh.arya37@gmail.com>
2023-06-14 02:22:53 +0530

    created a reusable component of sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { postContext } from "../contexts/PostContextProvider";
+
+export default function Sidebar() {
+  const { state } = useContext(postContext);
+  return (
+    <div className="sidebar">
+      <label htmlFor="search">Search</label>
+      <input
+        type="text"
+        name=""
+        id="search"
+        placeholder="Search people, post, anything"
+      />
+      <div>
+        <h2>Who to follow?</h2>
+        <button>
+          <span className="test">Following</span>
+          <span className="test2">Unfollow</span>
+        </button>
+        <ul>
+          {state?.allUsers?.map((item) => (
+            <li key={item._id}>
+              {item.username}
+              <button>Follow</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
