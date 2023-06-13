c38fc004b49246bb677b59e4e3b901e810d9018a
shubh-37 <shubh.arya37@gmail.com>
2023-06-14 02:21:58 +0530

    added navbar and sidebar components

--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -1,26 +1,33 @@
-import { useContext } from "react"
-import { postContext } from "../contexts/PostContextProvider"
+import { useContext } from "react";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import { postContext } from "../contexts/PostContextProvider";
+import "../css/explorepage.css";
 
-export default function Explore(){
+export default function Explore() {
   const { state } = useContext(postContext);
 
-  return(
+  return (
     <div className="explore-parent">
-      <h1>this is the explore page</h1>
-      <h2>Showing all posts</h2>
-      <ul>
-            {state?.allPosts?.map((item) => (
-              <li key={item._id}>
-                <h3>{item.username}</h3>
-                <p>{item.content}</p>     
-                <span className="	fa fa-heart-o"></span>
-                <span className="fa fa-comment-o"></span>
-                <span className="fa fa-share-alt"></span>
-                <span className="fa fa-bookmark-o"></span>
-              </li>
-            ))}
-          </ul>
+      <Navbar />
+      <div className="explore-main">
+        <h1>this is the explore page</h1>
+        <h2>Showing all posts</h2>
+        <ul>
+          {state?.allPosts?.map((item) => (
+            <li key={item._id}>
+              <h3>{item.username}</h3>
+              <p>{item.content}</p>
+              <span className="	fa fa-heart-o"></span>
+              <span className="fa fa-comment-o"></span>
+              <span className="fa fa-share-alt"></span>
+              <span className="fa fa-bookmark-o"></span>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <Sidebar />
     </div>
-    
-  )
+  );
 }
