e88df6e984f45a56d964e9e8cd0e0922b3b0f0c2
shubh-37 <shubh.arya37@gmail.com>
2023-06-14 05:36:51 +0530

    shifted the providers to fix a bug

--- src/index.js
+++ src/index.js
@@ -14,11 +14,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <AuthProvider>
-        <PostContextProvider>
+      <PostContextProvider>
+        <AuthProvider>
           <App />
-        </PostContextProvider>
-      </AuthProvider>
+        </AuthProvider>
+      </PostContextProvider>
     </Router>
   </React.StrictMode>
 );
