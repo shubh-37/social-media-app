92a9b44bb30aa9aec7fe95597ed81f8840344d61
shubh-37 <shubh.arya37@gmail.com>
2023-06-14 05:35:38 +0530

    added sidebar and navbar and showed user details

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,5 +1,35 @@
+// import { useContext } from "react"
+// import { authContext } from "../contexts/AuthContextProvider"
+import { useContext } from "react";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import { postContext } from "../contexts/PostContextProvider";
+import "../css/profilepage.css";
+
 export default function Profile(){
+  const { state } = useContext(postContext);
+  const user = localStorage.getItem("user");
+  const userData = JSON.parse(user);
+  console.log(userData);
   return (
-    <h1>This is the profile page</h1>
+    <div className="profile-parent">
+      <Navbar />
+      <div className="profile-main">
+        <h2>{`${userData.firstName} ${userData.lastName}`}</h2>
+        <p>{`@${userData.username}`}</p>
+        <p>{userData.following.length} following</p>
+        <p>{userData.followers.length} follower</p>
+        <ul>
+          {
+            state?.loggedUserPosts?.map(item => (
+              <li>
+                {item.content}
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+      <Sidebar />
+    </div>
   )
 }
