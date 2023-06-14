392a22aa449217dd376012092f3b88ee8b20f6a4
shubh-37 <shubh.arya37@gmail.com>
2023-06-14 05:34:38 +0530

    filtering logged in user's post and saving in the state

--- src/contexts/AuthContextProvider.jsx
+++ src/contexts/AuthContextProvider.jsx
@@ -1,16 +1,29 @@
 import axios from "axios";
+import { useContext } from "react";
 import { createContext } from "react";
+import { postContext } from "./PostContextProvider";
 
 export const authContext = createContext();
 
 export default function AuthProvider({ children }) {
+  const { state, dispatch } = useContext(postContext);
+
   async function guestLogin() {
     const guestUser = {
       username: "adarshbalika",
       password: "adarshBalika123",
     };
-    const response = await axios.post("/api/auth/login", guestUser);
-    console.log(response.data);
+    try {
+      const response = await axios.post("/api/auth/login", guestUser);
+      const value = JSON.stringify(response.data.foundUser);
+      localStorage.setItem("user", value);
+      const posts = state?.allPosts?.filter(
+        ({ username }) => username === response.data.foundUser.username
+      );
+      dispatch({ type: "ADD_USER_POST", payload: posts });
+    } catch (error) {
+      console.log(error);
+    }
   }
   return (
     <authContext.Provider value={{ guestLogin }}>
