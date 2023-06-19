406c6c7254f1b1298081ed3cda2f176257d606e6
shubh-37 <shubh.arya37@gmail.com>
2023-06-19 08:38:13 +0530

    added liked post and bookmark post functionality

--- src/Reducer.jsx
+++ src/Reducer.jsx
@@ -16,6 +16,32 @@ export default function reducer(state, action) {
         ...state,
         loggedUserPosts: action.payload,
       };
+    case "LIKE_POST": {
+      const post = action.payload.post?.reduce(
+        (acc, item) =>
+          item._id === action.payload.id
+            ? [
+                ...acc,
+                {
+                  ...item,
+                  likes: {
+                    ...item.likes,
+                    likedBy: [...item.likes.likedBy, action.payload.user],
+                  },
+                },
+              ]
+            : [...acc, {...item}],
+        []
+      );
+      return {
+        ...state,
+        allPosts: post,
+      };
+    }
+    case "BOOKMARK_POST": return {
+      ...state,
+      bookmarks: action.payload
+    }
     default:
       return state;
   }
