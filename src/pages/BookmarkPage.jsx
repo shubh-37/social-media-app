97e12744d9f08ab4202fbab0c0208859bdf7d976
shubh-37 <shubh.arya37@gmail.com>
2023-06-19 08:56:35 +0530

    displaying bookmarked post

--- src/pages/BookmarkPage.jsx
+++ src/pages/BookmarkPage.jsx
@@ -1,5 +1,38 @@
-export default function Bookmark(){
+import { useContext } from "react";
+import { postContext } from "../contexts/PostContextProvider";
+
+export default function Bookmark() {
+  const { state, likeHandler } = useContext(postContext);
+  const user = JSON.parse(localStorage.getItem("user"));
   return (
-    <h1>this is the bookmarks page</h1>
-  )
+    <div>
+      <h3>
+        {state.bookmarks.length === 0 && <p>There are no bookmarked posts</p>}
+      </h3>
+      <ul>
+        {state?.bookmarks?.map((item) => (
+          <li key={item._id}>
+            <h3>{item.username}</h3>
+            <p>{item.content}</p>
+            <div className="post-icons">
+              {item.likes.likedBy.includes(user.username) ? (
+                <span className="fa fa-heart">{item.likes.likeCount}</span>
+              ) : (
+                <span
+                  className="	fa fa-heart-o"
+                  onClick={() => likeHandler(item._id)}
+                >
+                  {item.likes.likeCount}
+                </span>
+              )}
+
+              <span className="fa fa-comment-o"></span>
+              <span className="fa fa-share-alt"></span>
+              <span className="fa fa-bookmark-o"></span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
