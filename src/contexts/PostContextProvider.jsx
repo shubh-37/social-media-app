317d83498794ed822222d84f31131c287916fb3a
shubh-37 <shubh.arya37@gmail.com>
2023-06-19 08:58:45 +0530

    api call to remove a post from bookmarks

--- src/contexts/PostContextProvider.jsx
+++ src/contexts/PostContextProvider.jsx
@@ -54,7 +54,6 @@ export default function PostContextProvider({ children }) {
   }
 
   async function bookmarkHandler(id){
-    console.log(id);
     try {
       const response = await fetch(`/api/users/bookmark/${id}`, {
         method: "POST",
@@ -69,12 +68,28 @@ export default function PostContextProvider({ children }) {
       console.log(error);
     }
   }
+
+  async function removeBookmarkHandler(id){
+    try {
+      const response = await fetch(`/api/users/remove-bookmark/${id}`, {
+        method: "POST",
+        headers: {
+          authorization: token
+        }
+      });
+      const data = await response.json();
+      const bookmarkedPost = state.allPosts.filter(item => data.bookmarks.includes(item._id));
+      dispatch({ type: "BOOKMARK_POST", payload: bookmarkedPost});
+    } catch (error) {
+      console.log(error);
+    }
+  }
   useEffect(() => {
     getAllPosts();
     getAllUsers();
   }, []);
   return (
-    <postContext.Provider value={{ state, dispatch, likeHandler, bookmarkHandler }}>
+    <postContext.Provider value={{ state, dispatch, likeHandler, bookmarkHandler, removeBookmarkHandler }}>
       {children}
     </postContext.Provider>
   );
