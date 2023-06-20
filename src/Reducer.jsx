e6b995aeb5ceeeab728cfb70751ef36b27505e2f
shubh-37 <shubh.arya37@gmail.com>
2023-06-20 15:57:12 +0530

    removed unnecessary code

--- src/Reducer.jsx
+++ src/Reducer.jsx
@@ -16,59 +16,26 @@ export default function reducer(state, action) {
         ...state,
         loggedUserPosts: action.payload,
       };
-    case "LIKE_POST": {
-      const post = action.payload.post?.reduce(
-        (acc, item) =>
-          item._id === action.payload.id
-            ? [
-                ...acc,
-                {
-                  ...item,
-                  likes: {
-                    ...item.likes,
-                    likedBy: [...item.likes.likedBy, action.payload.user],
-                  },
-                },
-              ]
-            : [...acc, { ...item }],
-        []
-      );
+    case "LIKE_POST":
       return {
         ...state,
-        allPosts: post,
+        allPosts: action.payload.post,
       };
-    }
     case "BOOKMARK_POST":
       return {
         ...state,
         bookmarks: action.payload,
       };
-    case "DISLIKE_POST": {
-      const post = action.payload.post?.reduce(
-        (acc, item) =>
-          item._id === action.payload.id
-            ? [
-                ...acc,
-                {
-                  ...item,
-                  likes: {
-                    ...item.likes,
-                    dislikedBy: [...item.likes.likedBy, action.payload.user],
-                  },
-                },
-              ]
-            : [...acc, { ...item }],
-        []
-      );
+    case "DISLIKE_POST":
       return {
         ...state,
-        allPosts: post,
+        allPosts: action.payload.post,
       };
-    }
-    case "USER": return {
-      ...state,
-      user: action.payload
-    }
+    case "USER":
+      return {
+        ...state,
+        user: action.payload,
+      };
     default:
       return state;
   }
