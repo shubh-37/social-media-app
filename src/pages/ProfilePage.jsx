f01c90f6c3aeaafb7129ea210ed878e5a0f710e9
shubh-37 <shubh.arya37@gmail.com>
2023-06-20 15:54:27 +0530

    modified some code related to state user and added logout btn

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,33 +3,29 @@
 import { useContext } from "react";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
+import { authContext } from "../contexts/AuthContextProvider";
 import { postContext } from "../contexts/PostContextProvider";
 import "../css/profilepage.css";
 
-export default function Profile(){
+export default function Profile() {
   const { state } = useContext(postContext);
-  const user = localStorage.getItem("user");
-  const userData = JSON.parse(user);
-  console.log(userData);
+  const { logoutUser } = useContext(authContext);
   return (
     <div className="profile-parent">
       <Navbar />
       <div className="profile-main">
-        <h2>{`${userData.firstName} ${userData.lastName}`}</h2>
-        <p>{`@${userData.username}`}</p>
-        <p>{userData.following.length} following</p>
-        <p>{userData.followers.length} follower</p>
+        <h2>{`${state.user?.firstName} ${state.user?.lastName}`}</h2>
+        <p>{`@${state.user?.username}`}</p>
+        <p>{state.user?.following?.length} following</p>
+        <p>{state.user?.followers?.length} follower</p>
         <ul>
-          {
-            state?.loggedUserPosts?.map(item => (
-              <li>
-                {item.content}
-              </li>
-            ))
-          }
+          {state?.loggedUserPosts?.map((item) => (
+            <li>{item.content}</li>
+          ))}
         </ul>
+        <button onClick={() => logoutUser()}>Logout</button>
       </div>
       <Sidebar />
     </div>
-  )
+  );
 }
