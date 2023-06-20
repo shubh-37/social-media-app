9a3b334c882f07ab6a7033beb4bb866cf9c463e3
shubh-37 <shubh.arya37@gmail.com>
2023-06-20 15:53:36 +0530

    added some comments

--- src/server.js
+++ src/server.js
@@ -55,19 +55,26 @@ export function makeServer({ environment = "development" } = {}) {
     routes() {
       this.namespace = "api";
       // auth routes (public)
+      //done
       this.post("/auth/signup", signupHandler.bind(this));
+      //done
       this.post("/auth/login", loginHandler.bind(this));
 
       // post routes (public)
+      //done
       this.get("/posts", getAllpostsHandler.bind(this));
+      //done
       this.get("/posts/:postId", getPostHandler.bind(this));
       this.get("/posts/user/:username", getAllUserPostsHandler.bind(this));
 
       // post routes (private)
+      //done
       this.post("/posts", createPostHandler.bind(this));
       this.delete("/posts/:postId", deletePostHandler.bind(this));
       this.post("/posts/edit/:postId", editPostHandler.bind(this));
+      //done
       this.post("/posts/like/:postId", likePostHandler.bind(this));
+      //done
       this.post("/posts/dislike/:postId", dislikePostHandler.bind(this));
 
       // user routes (public)
@@ -76,13 +83,18 @@ export function makeServer({ environment = "development" } = {}) {
 
       // user routes (private)
       this.post("users/edit", editUserHandler.bind(this));
+      //done
       this.get("/users/bookmark", getBookmarkPostsHandler.bind(this));
+      //done
       this.post("/users/bookmark/:postId/", bookmarkPostHandler.bind(this));
+      //done
       this.post(
         "/users/remove-bookmark/:postId/",
         removePostFromBookmarkHandler.bind(this)
       );
+      //done
       this.post("/users/follow/:followUserId/", followUserHandler.bind(this));
+      //done
       this.post(
         "/users/unfollow/:followUserId/",
         unfollowUserHandler.bind(this)
