65c602e819ad814dbb4b607c0f88545b860efcb8
shubh-37 <shubh.arya37@gmail.com>
2023-06-24 13:29:02 +0530

    added event handler to save the input results

--- src/components/ProfileModal.jsx
+++ src/components/ProfileModal.jsx
@@ -1,16 +1,30 @@
 import { useContext } from "react";
 import { postContext } from "../contexts/PostContextProvider";
-import "../css/profilemodal.css"
+import "../css/profilemodal.css";
 
-export default function ProfileModal() {
+export default function ProfileModal({
+  closeModal,
+  updateProfile,
+  saveProfile,
+}) {
   const { state } = useContext(postContext);
   return (
     <>
-      <div></div>
+      <div
+        style={{
+          position: "fixed",
+          width: "100%",
+          height: "100%",
+          top: "0",
+          left: "0",
+          opacity: "0.5",
+          backgroundColor: "grey",
+        }}
+      ></div>
       <div className="modal-background">
         <div className="modal-container">
           <div className="close-btn">
-            <button>X</button>
+            <button onClick={() => closeModal(false)}>X</button>
           </div>
           <div className="modal-header">
             <h3>Edit your personal information</h3>
@@ -22,6 +36,7 @@ export default function ProfileModal() {
               name="firstName"
               id=""
               defaultValue={state.user.firstName}
+              onChange={(e) => updateProfile(e)}
             />
             <label htmlFor="">Edit your last name: </label>
             <input
@@ -29,20 +44,30 @@ export default function ProfileModal() {
               name="lastName"
               id=""
               defaultValue={state.user.lastName}
+              onChange={(e) => updateProfile(e)}
             />
             <label htmlFor="">Add a bio</label>
-            <input type="text" name="bio" id="" defaultValue={state.user.bio} />
+            <input
+              type="text"
+              name="bio"
+              id=""
+              defaultValue={state.user.bio}
+              onChange={(e) => updateProfile(e)}
+            />
             <label htmlFor="">Add your portfolio link</label>
             <input
               type="text"
               name="portfolio_link"
               id=""
               defaultValue={state.user.portfolio_link}
+              onChange={(e) => updateProfile(e)}
             />
           </div>
           <div className="modal-footer">
-            <button>Cancel</button>
-            <button>Edit Profile</button>
+            <button onClick={() => closeModal(false)} className="cancel-btn">
+              Cancel
+            </button>
+            <button onClick={() => saveProfile()}>Edit Profile</button>
           </div>
         </div>
       </div>
