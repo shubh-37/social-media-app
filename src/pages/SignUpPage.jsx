87007572c27effaea1dc613ae13f37ceae382fe8
shubh-37 <shubh.arya37@gmail.com>
2023-06-28 02:10:32 +0530

    sign up functionality implementation and validation

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,20 +1,59 @@
+import { useContext } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { authContext } from "../contexts/AuthContextProvider";
 
 export default function SignUp() {
+  const { signUpUser } = useContext(authContext);
+  const [user, setUser] = useState({});
+  function userHandler(e) {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  }
+  function submitUser(e) {
+    e.preventDefault();
+    signUpUser(user);
+  }
   return (
     <div>
       <h1>This is the sign up page</h1>
-      <form>
+      <form onSubmit={(e) => submitUser(e)}>
         <label htmlFor="fname">First Name</label>
-        <input type="text" name="" id="fname" />
+        <input
+          type="text"
+          name="firstName"
+          id="fname"
+          required
+          onChange={(e) => userHandler(e)}
+        />
         <label htmlFor="lname">Last name</label>
-        <input type="text" name="" id="lname" />
+        <input
+          type="text"
+          name="lastName"
+          id="lname"
+          required
+          onChange={(e) => userHandler(e)}
+        />
         <label htmlFor="username">Username</label>
-        <input type="text" name="" id="username" />
+        <input
+          type="text"
+          name="username"
+          id="username"
+          required
+          onChange={(e) => userHandler(e)}
+        />
         <label htmlFor="password">Password</label>
-        <input type="password" name="" id="password" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          required
+          onChange={(e) => userHandler(e)}
+        />
         <label htmlFor="cpassword">Confirm Password</label>
-        <input type="password" name="" id="cpassword" />
+        <input type="password" name="" id="cpassword" required />
         <button type="submit">Sign up</button>
         <Link to="/login">Already have an account? Login now.</Link>
       </form>
