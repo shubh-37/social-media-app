240280a90eaa92941b2b77e7a7657974c32316bc
shubh-37 <shubh.arya37@gmail.com>
2023-06-27 18:22:46 +0530

    changed date format and content

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -17,7 +17,7 @@ export const posts = [
       dislikedBy: [],
     },
     username: "adarshbalika",
-    createdAt: formatDate(),
+    createdAt: new Date("August 30, 2021 03:15:30"),
     updatedAt: formatDate(),
   },
   {
@@ -30,7 +30,7 @@ export const posts = [
       dislikedBy: [],
     },
     username: "tanaypratap",
-    createdAt: formatDate(),
+    createdAt: new Date("June 27, 2023 03:15:30"),
     updatedAt: formatDate(),
   },
   {
@@ -43,7 +43,7 @@ export const posts = [
       dislikedBy: [],
     },
     username: "shubhamsoni",
-    createdAt: formatDate(),
+    createdAt: new Date("August 30, 2021 03:15:30"),
     updatedAt: formatDate(),
   },
   {
@@ -56,7 +56,7 @@ export const posts = [
       dislikedBy: [],
     },
     username: "shubhamsoni",
-    createdAt: formatDate(),
+    createdAt: new Date("August 30, 2021 03:15:30"),
     updatedAt: formatDate(),
   },
   {
@@ -69,7 +69,7 @@ export const posts = [
       dislikedBy: [],
     },
     username: "shubhamsoni",
-    createdAt: formatDate(),
+    createdAt: new Date("June 27, 2023 03:15:30"),
     updatedAt: formatDate(),
   },
   {
@@ -82,7 +82,7 @@ export const posts = [
       dislikedBy: [],
     },
     username: "tanaypratap",
-    createdAt: formatDate(),
+    createdAt: new Date("August 30, 2021 03:15:30"),
     updatedAt: formatDate(),
   },
   {
@@ -95,33 +95,33 @@ export const posts = [
       dislikedBy: [],
     },
     username: "tanaypratap",
-    createdAt: formatDate(),
+    createdAt: new Date("August 30, 2021 03:15:30"),
     updatedAt: formatDate(),
   },
   {
     _id: uuid(),
     content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+      "Im finished with all my features for the social media application that I'm working on!",
     likes: {
       likeCount: 8,
       likedBy: [],
       dislikedBy: [],
     },
     username: "shubh37",
-    createdAt: formatDate(),
+    createdAt: new Date("June 27, 2023 03:15:30"),
     updatedAt: formatDate(),
   },
   {
     _id: uuid(),
     content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+      "I'm so happy to get selected for the levelOne of neog camp. Can't wait to start this journey!!",
     likes: {
       likeCount: 15,
       likedBy: [],
       dislikedBy: [],
     },
     username: "shubh37",
-    createdAt: formatDate(),
+    createdAt: new Date("November 20, 2022 03:15:30"),
     updatedAt: formatDate(),
   },
   {
@@ -134,33 +134,31 @@ export const posts = [
       dislikedBy: [],
     },
     username: "shubhamsoni",
-    createdAt: formatDate(),
+    createdAt: new Date("August 30, 2021 03:15:30"),
     updatedAt: formatDate(),
   },
   {
     _id: uuid(),
-    content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+    content: "Just got done with an amazing session on React's useContext.",
     likes: {
       likeCount: 34,
       likedBy: [],
       dislikedBy: [],
     },
     username: "tanaypratap",
-    createdAt: formatDate(),
+    createdAt: new Date("April 27, 2023 03:15:30"),
     updatedAt: formatDate(),
   },
   {
     _id: uuid(),
-    content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+    content: "I just came back from an amazing trip in Mumbai. WohOOO!",
     likes: {
       likeCount: 55,
       likedBy: [],
       dislikedBy: [],
     },
     username: "mohitrana",
-    createdAt: formatDate(),
+    createdAt: new Date("February 27, 2023 03:15:30"),
     updatedAt: formatDate(),
   },
   {
@@ -173,59 +171,55 @@ export const posts = [
       dislikedBy: [],
     },
     username: "mohitrana",
-    createdAt: formatDate(),
+    createdAt: new Date("June 27, 2023 03:15:30"),
     updatedAt: formatDate(),
   },
   {
     _id: uuid(),
-    content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+    content: "My cryptography paper went well today I guess xD",
     likes: {
       likeCount: 6,
       likedBy: [],
       dislikedBy: [],
     },
     username: "prateekmishra",
-    createdAt: formatDate(),
+    createdAt: new Date("May 30, 2023 03:15:30"),
     updatedAt: formatDate(),
   },
   {
     _id: uuid(),
-    content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+    content: "Happy birthday shubh, have a great year!",
     likes: {
       likeCount: 33,
       likedBy: [],
       dislikedBy: [],
     },
     username: "mohitrana",
-    createdAt: formatDate(),
+    createdAt: new Date("October 30, 2022 03:15:30"),
     updatedAt: formatDate(),
   },
   {
     _id: uuid(),
-    content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+    content: "Happy 75th independence day everyone!",
     likes: {
       likeCount: 98,
       likedBy: [],
       dislikedBy: [],
     },
     username: "prateekmishra",
-    createdAt: formatDate(),
+    createdAt: new Date("August 15, 2022 03:15:30"),
     updatedAt: formatDate(),
   },
   {
     _id: uuid(),
-    content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+    content: "Happy republic day guys!",
     likes: {
       likeCount: 76,
       likedBy: [],
       dislikedBy: [],
     },
     username: "prateekmishra",
-    createdAt: formatDate(),
+    createdAt: new Date("January 26, 2021 03:15:30"),
     updatedAt: formatDate(),
   },
 
@@ -239,20 +233,19 @@ export const posts = [
       dislikedBy: [],
     },
     username: "sarasarya",
-    createdAt: formatDate(),
+    createdAt: new Date("August 30, 2021 03:15:30"),
     updatedAt: formatDate(),
   },
   {
     _id: uuid(),
-    content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+    content: "Happy birthday to myself! Season 29 begins...",
     likes: {
       likeCount: 99,
       likedBy: [],
       dislikedBy: [],
     },
     username: "sarasarya",
-    createdAt: formatDate(),
+    createdAt: new Date("September 06, 2022 03:15:30"),
     updatedAt: formatDate(),
   },
   {
@@ -265,59 +258,56 @@ export const posts = [
       dislikedBy: [],
     },
     username: "sarasarya",
-    createdAt: formatDate(),
+    createdAt: new Date("December 25, 2022 03:15:30"),
     updatedAt: formatDate(),
   },
   {
     _id: uuid(),
-    content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+    content: "Shit yaar phirse IPL me piche reh gaya",
     likes: {
       likeCount: 34,
       likedBy: [],
       dislikedBy: [],
     },
     username: "viratkohli",
-    createdAt: formatDate(),
+    createdAt: new Date("May 24, 2023 03:15:30"),
     updatedAt: formatDate(),
   },
   {
     _id: uuid(),
     content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+      "it's not shub, or subh, or sub. ITS SHUBH. How difficult is it to spell it right?",
     likes: {
       likeCount: 45,
       likedBy: [],
       dislikedBy: [],
     },
     username: "shubh37",
-    createdAt: formatDate(),
+    createdAt: new Date("June 24, 2023 03:15:30"),
     updatedAt: formatDate(),
   },
   {
     _id: uuid(),
-    content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+    content: "A hard fought WTC final, will come back stronger.",
     likes: {
       likeCount: 67,
       likedBy: [],
       dislikedBy: [],
     },
     username: "viratkohli",
-    createdAt: formatDate(),
+    createdAt: new Date("June 22, 2023 03:15:30"),
     updatedAt: formatDate(),
   },
   {
     _id: uuid(),
-    content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+    content: "Aao chalo firangiyo ki swatanra diwas manaye",
     likes: {
       likeCount: 22,
       likedBy: [],
       dislikedBy: [],
     },
     username: "adarshbalika",
-    createdAt: formatDate(),
+    createdAt: new Date("July 04, 2021 03:15:30"),
     updatedAt: formatDate(),
   },
   {
@@ -330,7 +320,7 @@ export const posts = [
       dislikedBy: [],
     },
     username: "mohitrana",
-    createdAt: formatDate(),
+    createdAt: new Date("March 30, 2021 03:15:30"),
     updatedAt: formatDate(),
   },
 ];
