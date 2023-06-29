8aa621f2795ed4bc919665fc5bb68df777080074
shubh-37 <shubh.arya37@gmail.com>
2023-06-29 18:23:36 +0530

    style: added image, added login icon, styled the landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,27 +1,83 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { postContext } from "../contexts/PostContextProvider";
+import "../css/landingpage.css";
 
-export default function LandingPage(){
-    return(
-      <div>
-        <div className="landing-content">
-          <h1>Shwitter</h1>
+export default function LandingPage() {
+  const { isDarkMode } = useContext(postContext);
+  return (
+    <div className="landing-parent">
+      <div className="landing-content">
+        <h1 style={{ color: "#2563eb", marginTop: "0" }}>Shwitter</h1>
+        <div>
           <div>
-            <h2>FOLLOW</h2>
-            <p>people around the globe</p>
+            <h2 style={{ color: isDarkMode ? "white" : "#475569" }}>
+              FOLLOW{" "}
+              <span
+                style={{
+                  fontSize: "small",
+                  color: isDarkMode ? "grey" : "black",
+                }}
+              >
+                people around the globe
+              </span>
+            </h2>
           </div>
           <div>
-            <h2>CONNECT</h2>
-            <p>with your friends</p>
+            <h2 style={{ color: isDarkMode ? "white" : "#475569" }}>
+              CONNECT{" "}
+              <span
+                style={{
+                  fontSize: "small",
+                  color: isDarkMode ? "grey" : "black",
+                }}
+              >
+                with your friends
+              </span>{" "}
+            </h2>
           </div>
           <div>
-            <h2>SHARE</h2>
-            <p>what you're thinking</p>
+            <h2 style={{ color: isDarkMode ? "white" : "#475569" }}>
+              SHARE{" "}
+              <span
+                style={{
+                  fontSize: "small",
+                  color: isDarkMode ? "grey" : "black",
+                }}
+              >
+                what you're thinking
+              </span>{" "}
+            </h2>
           </div>
-
-          <button><Link to="/signup">Join Now</Link></button>
-          <Link to="/login">Already have an account</Link>
         </div>
-        <div className="landing-img"></div>
+        <div>
+          <Link to="/signup" style={{ textDecoration: "none", color: "white" }}>
+            <button className="join-btn">Join Now</button>
+          </Link>
+          <Link
+            to="/login"
+            style={{
+              display: "block",
+              textDecoration: "none",
+              color: isDarkMode ? "white" : "black",
+              // textAlign: "center",
+              marginTop: "1rem",
+            }}
+          >
+            Already have an account?{" "}
+            <span
+              className="	fa fa-hand-o-left"
+              style={{ fontSize: "1.5rem" }}
+            ></span>
+          </Link>
+        </div>
       </div>
-    )
+      <div className="landing-img">
+        <img
+          src="https://i.ibb.co/F3J3zRg/karsten-winegeart-60-Gsd-OMRFGc-unsplash.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
