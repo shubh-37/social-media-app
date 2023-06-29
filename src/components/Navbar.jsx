14ef51f9307ebc5a6b2246712d5e19e6565198c6
shubh-37 <shubh.arya37@gmail.com>
2023-06-30 03:14:58 +0530

    imported stylesheet, implemented darkmode logic and restructured the code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,85 @@
+import { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
+import "../css/navbar.css";
+import { postContext } from "../contexts/PostContextProvider";
+
 export default function Navbar() {
+  const { isDarkMode } = useContext(postContext);
   return (
     <div className="home-navigate">
-      <NavLink to="/">
-        <span className="fa fa-home">Home</span>
-      </NavLink>
-      <NavLink to="/explore">
-        <span className="fa fa-rocket">Explore</span>
-      </NavLink>
-      <NavLink to="/bookmark">
-        <span className="fa fa-bookmark-o">Bookmarks</span>
-      </NavLink>
-      <NavLink to="/profile">
-        <span className="fa fa-user-o">Profile</span>
-      </NavLink>
-      <button>Create New post</button>
-      <Link to="/login">Login</Link>
+      <div style={{ marginLeft: "5rem" }}>
+        <NavLink
+          style={{
+            textDecoration: "none",
+            color: isDarkMode ? "white" : "black",
+          }}
+          to="/"
+        >
+          <span
+            style={{ border: isDarkMode ? "1px solid #2563eb" : "" }}
+            className="fa fa-home"
+          >
+            {" "}
+            Home
+          </span>
+        </NavLink>
+        <NavLink
+          style={{
+            textDecoration: "none",
+            color: isDarkMode ? "white" : "black",
+          }}
+          to="/explore"
+        >
+          <span
+            style={{ border: isDarkMode ? "1px solid #2563eb" : "" }}
+            className="fa fa-rocket"
+          >
+            {" "}
+            Explore
+          </span>
+        </NavLink>
+        <NavLink
+          style={{
+            textDecoration: "none",
+            color: isDarkMode ? "white" : "black",
+          }}
+          to="/bookmark"
+        >
+          <span
+            style={{ border: isDarkMode ? "1px solid #2563eb" : "" }}
+            className="fa fa-bookmark-o"
+          >
+            {" "}
+            Bookmarks
+          </span>
+        </NavLink>
+        <NavLink
+          style={{
+            textDecoration: "none",
+            color: isDarkMode ? "white" : "black",
+          }}
+          to="/profile"
+        >
+          <span
+            style={{ border: isDarkMode ? "1px solid #2563eb" : "" }}
+            className="fa fa-user-o"
+          >
+            {" "}
+            Profile
+          </span>
+        </NavLink>
+        <button className="create-post-btn">Create New post</button>
+
+        <Link
+          to="/login"
+          style={{
+            textDecoration: "none",
+            color: isDarkMode ? "white" : "black",
+          }}
+        >
+          <button className="login-btn">Login</button>
+        </Link>
+      </div>
     </div>
   );
 }
