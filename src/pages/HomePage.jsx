29800bf14d774621c346ca22b34a90ff71764e7c
shubh-37 <shubh.arya37@gmail.com>
2023-07-02 03:04:55 +0530

    added jump to top btn on scroll -test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,8 +7,17 @@ import { postContext } from "../contexts/PostContextProvider";
 import "../css/homepage.css";
 
 export default function HomePage() {
-  const { state, createPost, recentPosts, dispatch, isDarkMode } = useContext(postContext);
-  const [postContent, setPostContent] = useState("");
+  const {
+    state,
+    createPost,
+    recentPosts,
+    dispatch,
+    isDarkMode,
+    setPostContent,
+    postContent,
+  } = useContext(postContext);
+  const [isScroll, setIsScroll] = useState(false);
+
   function textChange(e) {
     setPostContent(e.target.value);
   }
@@ -19,12 +28,33 @@ export default function HomePage() {
   function recentHandler() {
     dispatch({ type: "RECENT" });
   }
+  window.onscroll = function (e) {
+    setIsScroll(true);
+  };
+  function clickHandler() {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+    // if(window.scroll(0,0)){
+    //   setIsScroll(false);
+    // }
+    setTimeout(() => setIsScroll(false), 500);
+  }
   return (
     <div className="home">
       <Navbar />
       <div className="main-content">
-      <h2 style={{margin: "1.5rem 1rem"}}>Home</h2>
-        <div className="create-post" style={{backgroundColor: isDarkMode ? "#bfdbfe": "#dbeafe", color: isDarkMode ? "black" : ""}}>
+        <h2 style={{ margin: "1.5rem 1rem" }}>Home</h2>
+
+        <div
+          className="create-post"
+          style={{
+            backgroundColor: isDarkMode ? "#bfdbfe" : "#dbeafe",
+            color: isDarkMode ? "black" : "",
+          }}
+        >
           <label htmlFor="new-post"></label>
           <input
             onChange={(e) => textChange(e)}
@@ -33,17 +63,30 @@ export default function HomePage() {
             id="new-post"
             placeholder="What is Happening!?"
             value={postContent}
-            style={{backgroundColor: isDarkMode ? "#bfdbfe": "#dbeafe", color: isDarkMode ? "black" : ""}}
+            style={{
+              backgroundColor: isDarkMode ? "#bfdbfe" : "#dbeafe",
+              color: isDarkMode ? "black" : "",
+            }}
           />
-          <div style={{borderTop : "1px solid"}}>
-            <span className="fa fa-image"></span>
-            <span className="fa fa-smile-o"></span>
+          <div
+            style={{
+              borderTop: "1px solid",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <div>
+              <span className="fa fa-image"></span>
+              <span className="fa fa-smile-o"></span>
+            </div>
+
             <button
               className="create-post-btn"
               onClick={() => {
                 createPost(postContent);
                 setPostContent("");
               }}
+              style={{ margin: "0.5rem", fontSize: "large" }}
             >
               Post
             </button>
@@ -70,6 +113,14 @@ export default function HomePage() {
               {state.recent ? "Showing recent" : "Recent posts"}
             </button>
           </div>
+          <div style={{display : "flex", width: "100%", alignItems: "center", justifyContent: "center"}}>
+            {isScroll && (
+              <button onClick={() => clickHandler()} className="scroll-btn">
+                Jump to Top
+              </button>
+            )}
+          </div>
+
           <ul>
             {recentPosts.map((item) => (
               <li key={item._id}>
