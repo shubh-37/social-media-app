d0b1f5c5bd3ac2294d739261d7e99f151470261b
shubh-37 <shubh.arya37@gmail.com>
2023-07-02 04:04:07 +0530

    added follow/following modal in the third person page -test

--- src/pages/ThirdPersonPage.jsx
+++ src/pages/ThirdPersonPage.jsx
@@ -5,27 +5,46 @@ import PostCard from "../components/PostCard";
 import Sidebar from "../components/Sidebar";
 import { postContext } from "../contexts/PostContextProvider";
 import "../css/thirdperson.css";
+import FollowModal from "../components/FollowModal";
 
 export default function ThirdPerson() {
   const { userId } = useParams();
-  const { state } = useContext(postContext);
+  const {
+    state,
+    followerModal,
+    followingModal,
+    setFollowerModal,
+    setFollowingModal,
+  } = useContext(postContext);
   const user = state.allUsers.find(({ username }) => username === userId);
-  console.log(user);
   return (
     <div className="tp-parent">
       <Navbar />
       <div className="profile-main">
-        <div className="avatar-img">
-          <img src={user?.avatar} alt="" />
+        <div style={{ textAlign: "center" }}>
+          <div className="avatar-img">
+            <img src={user?.avatar} alt="" />
+          </div>
+          <h2>{`${user?.firstName} ${user?.lastName}`}</h2>
+          <p>{`@${user?.username}`}</p>
+          <p
+            onClick={() => setFollowingModal(!followingModal)}
+            style={{ cursor: "pointer" }}
+          >
+            {user?.following?.length} following
+          </p>
+          <p
+            onClick={() => setFollowerModal(!followerModal)}
+            style={{ cursor: "pointer" }}
+          >
+            {user?.followers?.length} follower
+          </p>
+          <p>Bio: {user?.bio}</p>
+          <p>
+            <>Portfolio-url: {user?.portfolio_link}</>
+          </p>
         </div>
-        <h2>{`${user?.firstName} ${user?.lastName}`}</h2>
-        <p>{`@${user?.username}`}</p>
-        <p>{user?.following?.length} following</p>
-        <p>{user?.followers?.length} follower</p>
-        <p>Bio: {user?.bio}</p>
-        <p>
-          <>Portfolio-url: {user?.portfolio_link}</>
-        </p>
+
         <ul>
           {state?.allPosts
             ?.filter(({ username }) => username === user.username)
@@ -37,9 +56,12 @@ export default function ThirdPerson() {
         </ul>
       </div>
       <Sidebar />
+      {followerModal && (
+        <FollowModal closeModal={setFollowerModal} follower={true} />
+      )}
+      {followingModal && (
+        <FollowModal closeModal={setFollowingModal} follower={false} />
+      )}
     </div>
   );
 }
-
-//onClick={() => setFollowingModal(!followingModal)}
-//onClick={() => setFollowerModal(!followerModal)}
