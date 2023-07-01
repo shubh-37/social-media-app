819e53a094b224f4a5cf0efe5cef40a7c7f3c859
shubh-37 <shubh.arya37@gmail.com>
2023-07-02 03:44:10 +0530

    added styling to the modal elements

--- src/components/FollowModal.jsx
+++ src/components/FollowModal.jsx
@@ -2,31 +2,71 @@ import { useContext } from "react";
 import { postContext } from "../contexts/PostContextProvider";
 
 export default function FollowModal({ closeModal, follower }) {
-  const { state } = useContext(postContext);
+  const { state, isDarkMode } = useContext(postContext);
   return (
     <>
-      <div></div>
+      <div
+        style={{
+          position: "fixed",
+          width: "100%",
+          height: "100%",
+          opacity: "0.5",
+          backgroundColor: "grey",
+          top: "0",
+          left: "0",
+        }}
+      ></div>
       <div className="modal-background">
         <div className="modal-container">
           <div className="modal-header">
             <div className="close-btn">
               <button onClick={() => closeModal(false)}>X</button>
             </div>
-            <h3 style={{color: "black"}}>{follower ? "Followers" : "Following"}</h3>
+            <h3
+              style={{
+                color: "black",
+                marginTop: "0",
+                borderBottom: "1px solid",
+              }}
+            >
+              {follower ? "Followers" : "Following"}
+            </h3>
           </div>
           <div className="modal-body">
             <ul>
               {follower
                 ? state.user.followers.map((item) => (
-                    <li>
-                      <p>{`${item.firstName} ${item.lastName}`}</p>
+                    <li
+                      style={{
+                        display: "flex",
+                        margin: "0.5rem",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <p
+                        style={{
+                          color: isDarkMode ? "black" : "",
+                          fontSize: "large",
+                        }}
+                      >{`${item.firstName} ${item.lastName}`}</p>
                     </li>
                   ))
                 : state.user.following.map((item) => (
-                    <li>
-                      <p>{`${item.firstName} ${item.lastName}`}</p>
+                    <li
+                      style={{
+                        display: "flex",
+                        margin: "0.5rem",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <p
+                        style={{
+                          color: isDarkMode ? "black" : "",
+                          fontSize: "large",
+                        }}
+                      >{`${item.firstName} ${item.lastName}`}</p>
 
-                      <button>Following</button>
+                      <button className="create-post-btn">Following</button>
                     </li>
                   ))}
             </ul>
